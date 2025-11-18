612120b7bd1475c46936f261aaec4546eb43729c
Tomas Pavlatka <tomas@pavlatka.cz>
2025-11-18 07:24:25 +0100

    top k frequent element.

--- src/main/java/cz/pavlatka/leetcode/solutions/TopKFrequentElements.java
+++ src/main/java/cz/pavlatka/leetcode/solutions/TopKFrequentElements.java
@@ -11,9 +11,7 @@ public class TopKFrequentElements {
 
         var map = new HashMap<Integer, Integer>();
         for (int num: nums) {
-            var counter = map.getOrDefault(num, 0);
-            counter += 1;
-            map.put(num, counter);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         var win = new ArrayList<Integer>();
@@ -28,5 +26,58 @@ public class TopKFrequentElements {
         }
 
         return win.stream().mapToInt(i -> i).toArray();
+    }
+
+    public int[] buckets(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) {
+            return new int[]{};
+        }
+
+        var map = new HashMap<Integer, Integer>();
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        var groups = new HashMap<Integer, List<Integer>>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            groups.computeIfAbsent(entry.getValue(), i -> new ArrayList<>()).add(entry.getKey());
+        }
+
+        var list = groups.keySet().stream().sorted().toList();
+        var res = new ArrayList<Integer>();
+        for (int pos = list.size() - 1; pos >= 0 && res.size() < k; pos -= 1) {
+            res.addAll(groups.get(list.get(pos)));
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    public int[] bucketOn(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) {
+            return new int[]{};
+        }
+
+        var map = new HashMap<Integer, Integer>();
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] bucket = new List[nums.length+1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (bucket[entry.getValue()] == null) {
+                bucket[entry.getValue()] = new ArrayList<>();
+            }
+
+            bucket[entry.getValue()].add(entry.getKey());
+        }
+
+        var res = new ArrayList<Integer>();
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos -= 1) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
     }
 }
--- src/test/java/cz/pavlatka/leetcode/solutions/TopKFrequentElementsTest.java
+++ src/test/java/cz/pavlatka/leetcode/solutions/TopKFrequentElementsTest.java
@@ -37,4 +37,51 @@ class TopKFrequentElementsTest {
         Assertions.assertThat(result).contains(1, 2);
     }
 
+    @Test
+    void test4() {
+        var result = solver.buckets(new int[]{1,1,1,2,2,3}, 2);
+
+        Assertions.assertThat(result).hasSize(2);
+        Assertions.assertThat(result).contains(1, 2);
+    }
+
+    @Test
+    void test5() {
+        var result = solver.buckets(new int[]{1}, 1);
+
+        Assertions.assertThat(result).hasSize(1);
+        Assertions.assertThat(result).contains(1);
+    }
+
+    @Test
+    void test7() {
+        var result = solver.bucketOn(new int[]{1,2,1,2,1,2,3,1,3,2}, 2);
+
+        Assertions.assertThat(result).hasSize(2);
+        Assertions.assertThat(result).contains(1, 2);
+    }
+
+    @Test
+    void test8() {
+        var result = solver.bucketOn(new int[]{1,1,1,2,2,3}, 2);
+
+        Assertions.assertThat(result).hasSize(2);
+        Assertions.assertThat(result).contains(1, 2);
+    }
+
+    @Test
+    void test9() {
+        var result = solver.bucketOn(new int[]{1}, 1);
+
+        Assertions.assertThat(result).hasSize(1);
+        Assertions.assertThat(result).contains(1);
+    }
+
+    @Test
+    void test6() {
+        var result = solver.buckets(new int[]{1,2,1,2,1,2,3,1,3,2}, 2);
+
+        Assertions.assertThat(result).hasSize(2);
+        Assertions.assertThat(result).contains(1, 2);
+    }
 }
